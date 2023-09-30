b1fcc214401d34031e25a0ed3b552edc1cb13314
SYAN <syanays.git@gmail.com>
2023-09-30 04:02:31 +0300

    fix bg url

--- js/style-script.js
+++ js/style-script.js
@@ -1,8 +1,8 @@
 const bgButton = document.querySelector('.bg-button');
 const body = document.querySelector('body')
 
-const pinkBG = 'url(../media/purple-mountain-landscape.jpg)';
-const purpleBG = 'url(../media/3d-mountain-landscape-with-purple-sunset-sky.jpg)';
+const pinkBG = 'url(media/purple-mountain-landscape.jpg)';
+const purpleBG = 'url(media/3d-mountain-landscape-with-purple-sunset-sky.jpg)';
 const backgrounds = [pinkBG, purpleBG];
 let index = 1;
 bgButton.addEventListener('click', changeBackground)
